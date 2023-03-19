645c895c7dc6471a5f2b4b0858e29b652ef37491
amal ben ajiba <amalbenajiba11@gmail.com>
2023-03-19 08:42:20 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the digit base
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
+
