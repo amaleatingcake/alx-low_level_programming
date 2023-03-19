c092dd0591c8ee68b2661d5816d61627d76958b1
amal ben ajiba <amalbenajiba11@gmail.com>
2023-03-19 07:52:27 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - displau all the alphabet in lowercase exept q and e
+ * Return: Always (0)
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+	if (i != 'e' && i != 'q')
+		putchar (i);
+}
+putchar('\n');
+return (0);
+}
